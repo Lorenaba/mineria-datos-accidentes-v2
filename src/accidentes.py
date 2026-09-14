f572062b5b0e3861e8a7a9cfba5e7061aef09f03
Your Name <you@example.com>
2026-09-14 18:48:25 -0500

    Agregar carpeta src

---
+++ src/accidentes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+class Accidentes:
+
+    def __init__(self, ruta):
+        self.ruta = ruta
+        self.df = None
+
+    def cargar(self):
+        self.df = pd.read_csv(self.ruta)
+        return self.df
